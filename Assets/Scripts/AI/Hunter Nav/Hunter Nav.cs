6e128ac5a162fa34ad860509e0ad111f38942a19
MadFishie <125475305+MadFishie@users.noreply.github.com>
2024-01-25 14:59:12 -0500

    I hate this, I hate me....
    And I really really hate using lists in c#

---
+++ Assets/Scripts/AI/Hunter Nav/Hunter Nav.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class HunterNav : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private GameObject NavPoints;
+    private List<Transform> NavPointList=new List<Transform>();
+    private int NavPointCount;
+    private bool Hunting;
+    [SerializeField] private Transform player;
+    private NavMeshAgent agent;
+
+
+    void Start()
+    {
+
+        NavPointCount = NavPoints.transform.childCount;
+        //Debug.Log(NavPointCount);
+        //var navItems = NavPoints.GetComponentInChildren<Transform>();
+        //NavPointList = navItems;
+        
+       foreach (Transform child in NavPoints.transform)
+        {
+            Debug.Log(child);
+            NavPointList.Add(child);
+            Debug.Log("point added");
+        }
+        
+        /*for (int i = 0; i < NavPointCount; i++)
+        {
+            //Debug.Log(i);
+            Debug.Log(NavPoints.transform.GetChild(i));
+            NavPointList.Add(NavPoints.transform.GetChild(i));
+            Debug.Log("Child added");
+
+        }*/
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (NavTarget.TargetReached==true&&Hunting==false)
+        {
+
+            agent.destination = NavPointList[Random.Range(0, NavPointCount)].position;
+            NavTarget.TargetReached = false;
+
+        }
+        if (Hunting==true)
+        {
+            agent.destination = player.position;
+        }
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Hunting = true;
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        Hunting = false;
+        NavTarget.TargetReached = true;
+    }
+}
